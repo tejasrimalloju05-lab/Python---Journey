8153d929579b2df96db708d8e992b7cdc6ad2dba
Tejasri Malloju <tejasrimalloju05@gmail.com>
2026-09-20 12:06:30 +0530

    Add Python variables example

---
+++ Variables/variables.py
@@ -0,0 +1,17 @@
+# Python Variables
+
+name = "Ramu"
+age = 22
+cgpa = 9.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
+
+print("\nData Types:")
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
