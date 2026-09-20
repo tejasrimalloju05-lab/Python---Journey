1344275ea0c70635f8ab21f3d95dd6c62d97fa45
Tejasri Malloju <tejasrimalloju05@gmail.com>
2026-09-20 12:09:50 +0530

    Add Python data types example

---
+++ Data-Types/data_types.py
@@ -0,0 +1,29 @@
+# Python Data Types
+
+text = "Hello Python"
+number = 25
+decimal = 10.5
+active = True
+items = [1, 2, 3]
+coordinates = (10, 20)
+unique_values = {1, 2, 3}
+student = {"name": "Alex", "age": 21}
+
+print("String:", text)
+print("Integer:", number)
+print("Float:", decimal)
+print("Boolean:", active)
+print("List:", items)
+print("Tuple:", coordinates)
+print("Set:", unique_values)
+print("Dictionary:", student)
+
+print("\nData Types:")
+print(type(text))
+print(type(number))
+print(type(decimal))
+print(type(active))
+print(type(items))
+print(type(coordinates))
+print(type(unique_values))
+print(type(student))
